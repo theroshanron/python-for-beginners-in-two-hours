d2fe8bfe405665a7ff53d7bc20351180779d0182
theroshanron <95roshan1@gmail.com>
2021-12-20 23:11:37 +0530

    List, Tuples and Sets

---
+++ tutorials2.py
@@ -0,0 +1,37 @@
+#Lists
+"""Multiple items in 1 variable
+Can be updated
+Built in data type
+Used to store collection of datta
+created using []
+items are in orders with indexed
+New items are added at the end of the list with append"""
+
+list = ["a",2,True,"boom"]
+print(list)
+print(len(list))
+
+#Tuples
+""" Multiple items in 1 variable
+Build in data type
+Presented by ()
+Ordered but unchangeable"""
+
+tuple = ("organge","apple",2,2,3.5,True)
+print(len(tuple))
+print(tuple)
+
+single_tuple=("a",)
+print(single_tuple)
+
+#Sets
+""" Multiple items in 1 variable
+Build in data type
+written with {}
+unordered, unindexed
+duplicate items are not allowed"""
+
+set = {"apple",2,3.5,"cherry",2,3}
+
+print(set)
+print(len(set))
