2590648c7b2f3a8f53bd7d5f8274929b552112e5
theroshanron <95roshan1@gmail.com>
2021-12-20 23:19:41 +0530

    Conditional statements

---
+++ tutorials3.py
@@ -0,0 +1,25 @@
+#Conditional statements
+
+a = 100
+b = 200
+
+if b > a:
+    print("b king")
+elif b==a:
+    print("b and a are reall cool")
+else:
+    print("a is king")
+    # Follows the law of indention else gives error
+    #Elif
+
+
+#While loops
+""" Execute a set of statement until the condition is true"""
+
+i = 5
+while i < 4:
+    print(i)
+    i +=1
+    
+
+    
