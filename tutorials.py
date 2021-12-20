c3a6812375fe052c1cd8cab56b6667c73ee4fcee
theroshanron <95roshan1@gmail.com>
2021-12-20 22:47:22 +0530

    Comments and Variables

---
+++ tutorials.py
@@ -0,0 +1,25 @@
+#comments 
+# used to make code more readable and always starts with "#"
+
+print("Hello") #This is a comment
+
+""" This is a multi-line
+comment"""
+
+#Variable - > Can be used to assign the value
+x = 10
+print(x)
+
+y ="coding"
+print(y)
+
+#How to change the type with casting
+x=str(10)
+print(x)
+
+y=int("2")
+print(y)
+
+
+z=float(5)
+print(z)
