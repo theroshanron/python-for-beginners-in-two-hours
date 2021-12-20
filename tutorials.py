e6560b60e361a22e3c2523f2a742d32d96d57524
theroshanron <95roshan1@gmail.com>
2021-12-20 23:01:41 +0530

    Data types

--- tutorials.py
+++ tutorials.py
@@ -23,3 +23,68 @@ print(y)
 
 z=float(5)
 print(z)
+
+
+x = "coding boy"
+y = 'coding'
+
+print(x,y)
+#Variables are case sensitive
+
+X="Snake is python"
+print(x)
+
+#Data types
+"""   str -- > Txt
+    int -- > numbers  (Whole number cannot have decimals)
+    float -- > Decimal numbers 
+    list,tuple,range -- > Sequence 
+    bool -- > Boolean types like true & false """
+    
+x = 5
+print(type(x))
+
+x = 1
+y = 2.8
+print(type(x))
+print(type(y))
+
+print("Double quote text")
+print("""Double quote text as well as 
+      multiline text for us to display
+      subject's body to everyone """)
+
+#[] can be used to access elements of the character
+
+a = (" Hello World ")
+print(a[5])
+
+#Length of the string
+print(len(a))
+
+#Booleans - > Evaluation or Expression
+
+print (10 > 9)
+print (10 == 9)
+print (10 <= 9)
+#same results with the if statemets
+a = 100
+b = 50
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
+    
+print(bool("Hello"))
+print(bool(2))
+""" All valid statements are acceptable but should not have empty string, list or number 0, value of none and value of false"""
+
+print(bool(""))
+print(bool(0))
+print(bool(False))
+print(bool(None))
+
+
+
+
