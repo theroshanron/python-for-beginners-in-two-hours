1be1143cecdb9b1be56d98a73ea0d016516a7bd3
theroshanron <95roshan1@gmail.com>
2021-12-23 23:23:27 +0530

    Variables and input

--- python_tutorials_pwm/tutorial1.py
+++ python_tutorials_pwm/tutorial1.py
@@ -2,3 +2,21 @@
 #vnenv - Virtual environment
 
 print ("Hello world") #print is used to print a message or functions
+
+
+#Variables - We use variables to temporary store the values of variables
+#Assignment operator is used to assign a value to the variables
+
+age =20
+price = 19.99
+first_name ="Roshan" 
+is_online = False
+
+patient_name = "John smith"
+patient_age = 20
+new_patient = True
+
+print(patient_name,patient_age,new_patient)
+
+name  = input("What is your name boy? ")
+print("Hello  " + name) #string concatenated
